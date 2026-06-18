5809293d5cc4cf6938c799180139b0a8cb8fd854
Arthur <arthur@example.com>
2026-06-18 22:09:11 +0800

    Initial Commit

---
+++ AshotTests/AshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  AshotTests.swift
+//  AshotTests
+//
+//  Created by ai on 18/06/2026.
+//
+
+import Testing
+@testable import Ashot
+
+struct AshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
